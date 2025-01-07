8e947fac1f1691ec1ca71bf9015509c51642d1a0
Joubert Diego <jdrsjdrsoares@gmail.com>
2025-01-07 22:39:40 +0100

    correction access maintenance

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.RegisterBasicView.as_view(), name='register'),
     path('complete_registration/<int:customer_id>/', views.CompleteRegistrationView.as_view(), name='complete_registration'),
-    path('gestao/', views.login_view, name='login'),
+    path('gestao', views.login_view, name='gestao'),
     path('logout/', views.logout_view, name='logout'),
 ]
